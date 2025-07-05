5a7ab52def17d67b5898ecb72bdbab7df495a06e
Rana Akash <rana.akash0003@gmail.com>
2025-07-05 17:30:35 +0600

    refactor: Remove environment variable for TMDB API key and use hardcoded value

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,8 +1,7 @@
 export const TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
 export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
 
-export const TMDB_API_KEY =
-  import.meta.env.VITE_TMDB_API_KEY || "766bf879a9cb6abbb38ffc4998e86911";
+export const TMDB_API_KEY = "766bf879a9cb6abbb38ffc4998e86911";
 
 export const getImageBasePath = (imageSize) => {
   return `https://image.tmdb.org/t/p/${imageSize || "w342"}`;
